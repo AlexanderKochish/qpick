353667ddca113c3bfcd0bc4bccba76b2a96bf11c
Alexahder Kochish <shvepsolek@gmail.com>
2025-12-06 19:57:44 +0000

    fix: middleware

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   const sessionToken = request.cookies.get('next-auth.session-token')
   const secureSessionToken = request.cookies.get(
-    '__Secure-next-auth.session-token'
+    '__Secure-authjs.session-token'
   )
 
   const isAuthenticated = !!(sessionToken || secureSessionToken)
