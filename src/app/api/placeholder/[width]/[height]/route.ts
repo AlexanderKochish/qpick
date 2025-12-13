5718c7870e5aabc2820d3cd4ff87d93007adcfd4
Alexahder Kochish <shvepsolek@gmail.com>
2025-12-13 00:43:23 +0000

    fix: api

--- src/app/api/placeholder/[width]/[height]/route.ts
+++ src/app/api/placeholder/[width]/[height]/route.ts
@@ -1,14 +1,15 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import sharp from 'sharp'
 import { readFile } from 'fs/promises'
 import path from 'path'
 
 export async function GET(
-  req: Request,
-  { params }: { params: { width: string; height: string } }
+  req: NextRequest,
+  { params }: { params: Promise<{ width: string; height: string }> }
 ) {
-  const width = Number(params.width) || 300
-  const height = Number(params.height) || 300
+  const { width: widthParam, height: heightParam } = await params
+  const width = Number(widthParam) || 300
+  const height = Number(heightParam) || 300
 
   const filePath = path.join(process.cwd(), 'public', 'no-image.jpg')
   const fileBuffer = await readFile(filePath)
